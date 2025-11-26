d41ebdc520adfcbb8a8916b2168641b74ef21561
JiNT1 <tommyjin2001@gmail.com>
2025-11-26 21:15:55 +0100

    solve the problem of publish manually initial pose, now it reads from odom

--- navigation_package/src/Lifecycle_node.cpp
+++ navigation_package/src/Lifecycle_node.cpp
@@ -3,8 +3,15 @@
 //client node
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "nav2_msgs/srv/manage_lifecycle_nodes.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "tf2_msgs/msg/tf_message.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp" 
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
 
 #include "interfaces_assignment_1/msg/ready.hpp"
 
@@ -15,8 +22,8 @@
 
 using namespace std::chrono_literals;
 
-class Lifecycle_node : public rclcpp::Node,
-                    public std::enable_shared_from_this<Lifecycle_node>
+class Lifecycle_node : public rclcpp::Node
+                    //public std::enable_shared_from_this<Lifecycle_node>
 {
     public:
     Lifecycle_node()
@@ -26,103 +33,151 @@ class Lifecycle_node : public rclcpp::Node,
         navigation_ptr = this->create_client<nav2_msgs::srv::ManageLifecycleNodes>("/lifecycle_manager_navigation/manage_nodes");
         initial_pose_pub = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/initialpose", 10);
         ready_pub = this->create_publisher<interfaces_assignment_1::msg::Ready>("/ready",10);
+        odom_sub = this->create_subscription<nav_msgs::msg::Odometry>("/odom",10, std::bind(&Lifecycle_node::odom_callback, this, std::placeholders::_1));
 
-        start_localization();
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Lifecycle node initialized");
+        initialpose_sent = false;
+        
     }
-
-    //void bringup()
-    //{   
-    //    start_localization();
-    //}
     
     private:
 
-    void start_localization()
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
+        if(!initialpose_sent)
+        {
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Received initial pose");
+        
+            geometry_msgs::msg::PoseWithCovarianceStamped initialPose;
+
+            initialPose.header.frame_id = "map";
+            initialPose.header.stamp = msg->header.stamp;
+            initialPose.pose = msg->pose;
+
+            //initial_pose_pub->publish(initialPose);
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Initial pose is set");
+
+            initialpose_sent = true;
+            
+            this->start_localization(initialPose);
+        }
+        return;
+    }
+
+    void start_localization(const geometry_msgs::msg::PoseWithCovarianceStamped &initialPose)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Start localization");
+
+        auto request = std::make_shared<nav2_msgs::srv::ManageLifecycleNodes::Request>();
+        request->command = 0; //nav2_msgs::srv::ManageLifecycleNodes::Request::STARTUP;
+
+
         while (!(localization_ptr)->wait_for_service()) {
             if (!rclcpp::ok()) {
                 RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the localization service. Exiting.");
+                this->publish_ready_msg(false);
                 return;
             }
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Localization service not available, waiting again...");
         }
+        
+
+        localization_ptr->async_send_request(request, [this, initialPose](rclcpp::Client<nav2_msgs::srv::ManageLifecycleNodes>::SharedFuture future)
+        {
+            try
+            {
+                auto response = future.get();
+                if(response->success)
+                {
+                    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "localization activated successfully");
+                    initial_pose_pub->publish(initialPose);
+                    this->start_navigation();
+                }else 
+                {
+                    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service Localization");
+                    this->publish_ready_msg(false);
+                }
+            }catch (const std::exception& e)
+            {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Exception in localization: %s", e.what());
+                this->publish_ready_msg(false);
+            }
+        });
+
+        return;
+
+    }
+
+    
+
+    void start_navigation()
+    {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Start navigation");
 
         auto request = std::make_shared<nav2_msgs::srv::ManageLifecycleNodes::Request>();
-        request->command = 0;//nav2_msgs::srv::ManageLifecycleNodes::Request::STARTUP;
-        auto localization_result = localization_ptr->async_send_request(request);
+        request->command = 0; //nav2_msgs::srv::ManageLifecycleNodes::Request::STARTUP;
 
-        auto message = interfaces_assignment_1::msg::Ready();
-        message.info.stamp = this->now();
-        message.info.frame_id =  "navigation";
-
-        if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), localization_result) == rclcpp::FutureReturnCode::SUCCESS)
-        {
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "localization result: %s", localization_result.get()->success == 1 ? "true" : "false");
-            start_navigation(message);
-        } else {
-            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service Localization");
-            message.ready.data = false;
-        }
-
-        ready_pub->publish(message);
-    }
-
-    void pub_initial_pose()
-    {
-        geometry_msgs::msg::PoseWithCovarianceStamped initialPose;
-        initialPose.header.frame_id = "map";
-        initialPose.pose.pose.position.x = 0.0;
-        initialPose.pose.pose.position.y = 0.0;
-        initialPose.pose.pose.position.z = 0.0;
-        initialPose.pose.pose.orientation.z = 0.0;
-        initialPose.pose.pose.orientation.w = 1.0;
-        std::array<double, 36> cov {
-                        0.25, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.25, 0.0, 0.0, 0.0, 0.0,
-                        0.0, 0.0, 0.068, 0.0, 0.0, 0.0,
-                        0.0, 0.0, 0.0, 0.068, 0.0, 0.0,
-                        0.0, 0.0, 0.0, 0.0, 0.068, 0.0,
-                        0.0, 0.0, 0.0, 0.0, 0.0, 0.068
-        };
-        for(int i = 0; i < static_cast<int>(cov.size()); i++)
-            initialPose.pose.covariance[i] = cov[i];
-
-        initial_pose_pub->publish(initialPose);
-    }
-
-    void start_navigation(interfaces_assignment_1::msg::Ready &msg)
-    {
-
-        pub_initial_pose();
 
         while (!(navigation_ptr)->wait_for_service()) {
             if (!rclcpp::ok()) {
                 RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the navigation service. Exiting.");
+                this->publish_ready_msg(false);
                 return;
             }
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Navigation service not available, waiting again...");
         }
         
-        auto request = std::make_shared<nav2_msgs::srv::ManageLifecycleNodes::Request>();
-        request->command = 0;//nav2_msgs::srv::ManageLifecycleNodes::Request::STARTUP;
-        auto navigation_result = navigation_ptr->async_send_request(request);
-
-        if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), navigation_result) == rclcpp::FutureReturnCode::SUCCESS)
+        
+        navigation_ptr->async_send_request(request, [this](rclcpp::Client<nav2_msgs::srv::ManageLifecycleNodes>::SharedFuture future)
         {
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "navigation result: %s", navigation_result.get()->success == 1 ? "true" : "false");
-            msg.ready.data = true;
-        } else {
-            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service navigation");
-            msg.ready.data = false;
-        }
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            try
+            {
+                auto response = future.get();
+                if (response->success) 
+                {
+                    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "navigation activated successfully");
+                    odom_sub.reset();
+                    this->publish_ready_msg(true);
+                }else
+                {
+                    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service navigation");
+                    this->publish_ready_msg(false);
+                }
+            }catch(const std::exception& e)
+            {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Exception in navigation: %s", e.what());
+                this->publish_ready_msg(false);
+            }
+        });
 
-        ready_pub->publish(msg);
+        return;
+    }
+
+    void publish_ready_msg(bool ready)
+    {
+        auto message = interfaces_assignment_1::msg::Ready();
+        message.info.stamp = this->now();
+        message.info.frame_id =  "navigation";
+        message.ready.data = ready;
+        ready_pub->publish(message);
+
+        if (ready) {
+            RCLCPP_INFO(this->get_logger(), "Robot is ready to move!");
+        } else {
+            RCLCPP_ERROR(this->get_logger(), "Nav2 bringup sequence failed!");
+        }
     }
     
     rclcpp::Client<nav2_msgs::srv::ManageLifecycleNodes>::SharedPtr localization_ptr;
     rclcpp::Client<nav2_msgs::srv::ManageLifecycleNodes>::SharedPtr navigation_ptr;
     rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr initial_pose_pub;
     rclcpp::Publisher<interfaces_assignment_1::msg::Ready>::SharedPtr ready_pub;
-    //rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    bool initialpose_sent;
+    //rclcpp::TimerBase::SharedPtr retry_timer_;
 };
 
 int main(int argc, char ** argv)
